019ff998b5159c48cdb45ccf644ea28427ece54c
mridula.chappalli.srinivasa@sv.cmu.edu <aadityanagendra@localhost.localdomain>
2013-09-06 23:53:11 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carnaticraga::Application.config.secret_key_base = 'e60d7db222d709eef72f8c918ee4330d04b0f3c7891c3ea20dad967083e256d2ea22fc319de9b52ca864bf7be17806cf3a033451c04d54773c9c2611442c2e52'
